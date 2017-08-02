11dcb8bb24a3eb381c68cfac8bf9e01decabd570
Ryan Johnson <ryanmarkjohnson920@gmail.com>
2017-08-01 19:50:35 -0700

    Add Hello World dotnet console app and jenkinsfile.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CDTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
